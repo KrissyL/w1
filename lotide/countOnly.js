1c33c8967c7d5f5e3869d28b2ec5f2ea57b78223
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-21 22:59:32 +0000

    forgot to lint

--- lotide/countOnly.js
+++ lotide/countOnly.js
@@ -1,39 +1,39 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-      console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+  if (actual === expected) {
+    console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 const countOnly = function(allItems, itemsToCount) {
-    let results = {};
-    for (const item of allItems) {
-        if (itemsToCount[item]) {
-            if (results[item]) {
-                results[item] += 1;
-            } else {
-                results[item] = 1;
-            }
-        }
+  let results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
-    return results;
+  }
+  return results;
 };
 
 const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
   
-  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
-  console.log(result1);
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+console.log(result1);
   
-  assertEqual(result1["Jason"], 1);
-  assertEqual(result1["Karima"], undefined);
-  assertEqual(result1["Fang"], 2);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
