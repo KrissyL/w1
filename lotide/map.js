cc8066c6dff352b1f5d73f3160f4f82865c6b5f1
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-23 00:19:12 +0000

    a map function that returns the first idex of an element

---
+++ lotide/map.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✔️ Assertion Passed: ${arr1} & ${arr2} are equal!`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} & ${arr2} are not equal!`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const words2 = ["kris", "bea", "ann", "lew"];
+const words3 = ["Battle", "Ends", "And", "Down", "Goes", "Charles's","Father"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words2, word => word[0]);
+const results3 = map(words3, word => word[0]);
+console.log(results1);
+console.log(results2);
+console.log(results3);
