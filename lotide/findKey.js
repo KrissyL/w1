ae977eb469ea4a6b2c7b5c76693d3376bda50b85
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-23 02:23:29 +0000

    a function to find the key of an object

---
+++ lotide/findKey.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const findKey = function(obj, callback) {
+  for (const objKey in obj) {
+    if (callback(obj[objKey])) {
+      return objKey;
+    }
+  }
+};
+const someObj = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+assertEqual(findKey(someObj, x => x.stars === 2), "noma"); // => "noma"
