49507822f5c40e71c616dfaf796c3e2cb0b9ccb5
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-26 19:39:28 +0000

    refactored tail function

--- lotide/tail.js
+++ lotide/tail.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('../assertEqual');
 
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+modole.exports = tail;
