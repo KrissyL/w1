a6843c97b3d2f5ebe4d1738f68920cb818ad84b8
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-26 19:54:45 +0000

    more refactoring!

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
-    } else if (actual !== expected) {
-      console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+const assertEqual = require('./assertEqual');
 
   const eqArrays = function(arr1, arr2) {
       if (arr1.length !== arr2.length) {
@@ -19,7 +13,4 @@ const assertEqual = function(actual, expected) {
           return true;
   };
 
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  module.exports = eqArrays;
--- lotide/head.js
+++ lotide/head.js
@@ -1,5 +1,7 @@
-const assertEqual = require('../assertEqual');
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   return arr[0];
 };
+
+module.exports = head;
