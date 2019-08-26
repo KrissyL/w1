57fb3735a25d8a9b6f50864bb37d8e6d211576dd
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-26 22:17:52 +0000

    testing lotide functions with chai

---
+++ lotide/test/middleTest.js
@@ -0,0 +1,18 @@
+const assert = require('chai').assert;
+
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("return [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(middle(['5']), []);
+  });
+  it("returns ['Lighthouse'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(middle(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse']);
+  });
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3])
+  });
+});
